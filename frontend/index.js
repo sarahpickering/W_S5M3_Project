578be9c1e7ace75763108ea20020359216e83f70
Sarah Pickering <sarahpsellsaz@gmail.com>
2024-08-24 08:49:50 -0700

    S5M3 Project working on building nav and footer components and using event listeners

--- frontend/index.js
+++ frontend/index.js
@@ -3,8 +3,15 @@ function moduleProject3() {
   // 👉 TASK 1 - Write a `buildNav` component that returns a nav
 
   function buildNav(links) {
-    //  ✨ do your magic here
-    return document.createElement('nav')
+    const container = document.createElement('nav')
+    links.forEach(link => {
+      let a = document.createElement('a')
+      a.href = link.href
+      a.title = link.title
+      a.textContent = link.textContent
+      container.appendChild(a)
+    })
+    return container
   }
 
   // ❗ DOM creation using your `buildNav` component (do not change):
@@ -19,7 +26,35 @@ function moduleProject3() {
   // 👉 TASK 2A - Write a `buildLearnerCard` component that returns a card
 
   function buildLearnerCard(learner, languages) {
-    //  ✨ do your magic here
+    const card = document.createElement('div')
+    card.classList.add('learner-card')
+
+    const nameP = document.createElement('p')
+    nameP.textContent = learner.fullName
+
+    const idP = document.createElement('p')
+    idP.textContent = `Learner ID: ${learner.id}`
+
+    const dobP = document.createElement('p')
+    dobP.textContent = `Date of Birth: ${learner.dateOfBirth}`
+
+    const favLangP = document.createElement('p')
+    const favLanguage = languages.find(lang => lang.id === learner.favLanguage)
+    favLangP.textContent = `Favorite Language: ${favLanguage.name}`;
+
+    [nameP, idP, dobP, favLangP].forEach(p => {
+      card.appendChild(p)
+
+      card.addEventListener('click', evt => {
+        document.querySelectorAll('.learner-card').forEach(card => {
+          card.classList.remove('active')
+        })
+        card.classList.add('active')
+      })
+
+    })
+
+    return card
   }
 
   {
@@ -39,14 +74,62 @@ function moduleProject3() {
       { id: 41, fullName: 'Sabah Beydoun', dateOfBirth: '1988-03-25', favLanguage: 91 },
       { id: 17, fullName: 'Daniel Castillo', dateOfBirth: '1995-11-05', favLanguage: 12 }
     ]
-    //  ✨ do your magic here
+
+    learners.forEach(learner => {
+      const learnerCard = buildLearnerCard(learner, languages)
+      document.querySelector('section').appendChild(learnerCard)
+    })
+
   }
+    
 
   // 👉 TASK 3 - Write a `buildFooter` component that returns a footer
 
   function buildFooter(footerData) {
     //  ✨ do your magic here
-    return document.createElement('footer')
+    const footer = document.createElement('footer')
+
+    let companyInfoDiv = document.createElement('div')
+    companyInfoDiv.classList.add('company-info')
+
+    let companyNameP = document.createElement('p')
+    companyNameP.classList.add('company-name')
+    companyNameP.textContent = footerData.companyName
+
+    let addressP = document.createElement('p')
+    addressP.classList.add('address')
+    addressP.textContent = footerData.address
+
+    let contactEmailP = document.createElement('p')
+    contactEmailP.classList.add('contact-email')
+    contactEmailP.innerHTML = `Email: <a href="mailto:${footerData.contactEmail}"> ${footerData.contactEmail}</a>`;
+
+
+  companyInfoDiv.appendChild(companyNameP)
+  companyInfoDiv.appendChild(addressP)
+  companyInfoDiv.appendChild(contactEmailP)
+
+  let socialMediaDiv = document.createElement('div')
+  socialMediaDiv.classList.add('social-media')
+
+  for (let platform in footerData.socialMedia) {
+    let socialMediaLink = document.createElement('a')
+    socialMediaLink.href = footerData.socialMedia[platform]
+    socialMediaLink.textContent = platform.charAt(0).toUpperCase() + platform.slice(1)
+    socialMediaDiv.appendChild(socialMediaLink)
+  }
+
+  let currentYear = new Date().getFullYear()
+  let copyright = document.createElement('div')
+  copyright.textContent = `© ${footerData.companyName.toUpperCase()} ${currentYear}`
+
+  
+  footer.appendChild(companyInfoDiv)
+  footer.appendChild(socialMediaDiv)
+  footer.appendChild(copyright)
+
+    return footer
+    //return document.createElement('footer')
   }
 
   // ❗ DOM creation using your `buildFooter` component (do not change):
@@ -64,6 +147,12 @@ function moduleProject3() {
   // 👉 TASK 4 - Clicking on the section should deactivate the active card
 
   //  ✨ do your magic here
+  document.addEventListener('click', evt => {
+    if (evt.target === document.querySelector('section')) {
+      const learners = document.querySelectorAll('.learner-card')
+      learners.forEach(card => card.classList.remove('active'))
+    }
+  })
 }
 
 // ❗ DO NOT CHANGE THIS CODE
